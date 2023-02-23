5d8581f477cca63d45ecadba85767af0cd0dce33
Ajay-oraon <52890890+Ajay-oraon@users.noreply.github.com>
2023-02-23 14:42:25 +0530

    Updates

---
+++ precache-manifest.00d4d8b14320a45b247ab129e49b0c7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fae5d8e37019978e223f",
+    "url": "/my-app/static/css/main.f2fd34bb.chunk.css"
+  },
+  {
+    "revision": "fae5d8e37019978e223f",
+    "url": "/my-app/static/js/main.fae5d8e3.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/my-app/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/my-app/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "8b16e4071cc3418912ee",
+    "url": "/my-app/static/js/runtime~main.8b16e407.js"
+  },
+  {
+    "revision": "221e38949b18c70f71585d9a30191d62",
+    "url": "/my-app/index.html"
+  }
+];
